81d351172702653404230771728bd3abd96ca46e
Мария Лапикова <mashik_p@mail.ru>
2022-12-02 01:35:56 +0300

    change tabs to spaces in indention

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -4,41 +4,41 @@ from rest_framework import viewsets, permissions
 from rest_framework.permissions import IsAuthenticated
 
 from .serializers import (
-	PostSerializer,
-	GroupSerializer, CommentSerializer
+    PostSerializer,
+    GroupSerializer, CommentSerializer
 )
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
-	def has_object_permission(self, request, view, obj):
-		if request.method in permissions.SAFE_METHODS:
-			return True
-		return obj.author == request.user
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user
 
 
 class PostViewSet(viewsets.ModelViewSet):
-	queryset = Post.objects.all()
-	serializer_class = PostSerializer
-	permission_classes = [IsAuthorOrReadOnly, IsAuthenticated]
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthorOrReadOnly, IsAuthenticated]
 
-	def perform_create(self, serializer):
-		serializer.save(author=self.request.user)
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
 
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
-	queryset = Group.objects.all()
-	serializer_class = GroupSerializer
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
 
 
 class CommentViewSet(viewsets.ModelViewSet):
-	queryset = Comment.objects.all()
-	serializer_class = CommentSerializer
-	permission_classes = [IsAuthorOrReadOnly, IsAuthenticated]
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthorOrReadOnly, IsAuthenticated]
 
-	def get_queryset(self):
-		post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
-		return Comment.objects.filter(post=post)
+    def get_queryset(self):
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        return Comment.objects.filter(post=post)
 
-	def perform_create(self, serializer):
-		post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
-		serializer.save(author=self.request.user, post=post)
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        serializer.save(author=self.request.user, post=post)
